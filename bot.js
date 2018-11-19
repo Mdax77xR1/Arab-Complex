08e67dbb8ba526a32da1070d54009d8658e507c9
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-19 06:24:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -930,18 +930,49 @@ client.guilds.forEach(g => {
   });
 });
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "513065026907537410");
-    logChannel.send(`Invited by: <@${inviter.tag}>`);
-  });
+
+
+
+
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("اي دي سيرفرك - Server id");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
 });
-
-
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("513065026907537410");
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("494920525051592705");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
 
 
 
