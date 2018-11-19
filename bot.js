9e6730bbfdb8db02109176af96d8212f62cc6df1
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-19 06:09:27 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -922,18 +922,24 @@ client.on('ready', () => {
   });
 });
 
+
+client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
 client.on('guildMemberAdd', member => {
   member.guild.fetchInvites().then(guildInvites => {
     const ei = invites[member.guild.id];
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "𝑪𝑯𝑨𝑻");
+    const logChannel = member.guild.channels.find(channel => channel.name === "513065026907537410");
     logChannel.send(`Invited by: <@${inviter.tag}>`);
   });
 });
-
-
 
 
 
